45d7dc7c82fe7f71879d7f34becb1a9504728145
TureNate <145368988+TureNate@users.noreply.github.com>
2023-10-18 20:29:00 +0500

    Add files via upload

---
+++ aboba1.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class aboba1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Debug.Log("Hello, World!");
+        
+        // Start is called before the first frame update
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
